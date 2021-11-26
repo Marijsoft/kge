a49bcabd467671f05541713c05b6c25f0c9a5f37
Marijsoft <marijsoftdevteam@gmail.com>
2021-11-26 02:15:33 +0100

    Add files via upload

--- keygenexcel.cpp
+++ keygenexcel.cpp
@@ -7,12 +7,13 @@
 #pragma argsused
 #include <tchar.h>
 #include <stdio.h>
+using namespace std;
 
-void vainfiga(std::string&);
+void vainfiga(string&);
 
-void vainfiga(std::string& text) {
-	std::transform(begin(text), end(text), begin(text), [](char c)->char {
-		if (not std::isalpha(c))
+void vainfiga(string& text) {
+	transform(begin(text), end(text), begin(text), [](char c)->char {
+		if (not isalpha(c))
 			return c;
 
 		char const pivot = std::isupper(c) ? 'A' : 'a';
@@ -21,10 +22,10 @@ void vainfiga(std::string& text) {
 
 int _tmain(int argc, _TCHAR* argv[]) {
 
-	std::string email;
-	std::cout << "Keygen per Excel2MySQL"<< std::endl;
-	std::cout << "Email: ";
-	std::cin >> email;
+	string email;
+	cout << "Keygen per Excel2MySQL"<< std::endl;
+	cout << "Email: ";
+	cin >> email;
 
 	int cnt = 0;
 	for (auto i : email) {
@@ -36,16 +37,16 @@ int _tmain(int argc, _TCHAR* argv[]) {
 		licenza[cnt++] = toupper(i);
 	}
 	vainfiga(licenza);
-	std::replace(licenza.begin(), licenza.end(), '0', 'C');
-	std::replace(licenza.begin(), licenza.end(), 'O', 'K');
-	std::replace(licenza.begin(), licenza.end(), 'I', '3');
-	std::replace(licenza.begin(), licenza.end(), '1', '7');
-	std::replace(licenza.begin(), licenza.end(), 'L', 'T');
-	std::replace(licenza.begin(), licenza.end(), 'S', 'W');
-	std::replace(licenza.begin(), licenza.end(), 'B', 'V');
-	std::replace(licenza.begin(), licenza.end(), '8', '4');
-	std::replace(licenza.begin(), licenza.end(), 'Z', 'X');
-	std::cout << "Licenza : " << licenza.substr(0, 15) << std::endl;
+	replace(licenza.begin(), licenza.end(), '0', 'C');
+	replace(licenza.begin(), licenza.end(), 'O', 'K');
+	replace(licenza.begin(), licenza.end(), 'I', '3');
+	replace(licenza.begin(), licenza.end(), '1', '7');
+	replace(licenza.begin(), licenza.end(), 'L', 'T');
+	replace(licenza.begin(), licenza.end(), 'S', 'W');
+	replace(licenza.begin(), licenza.end(), 'B', 'V');
+	replace(licenza.begin(), licenza.end(), '8', '4');
+	replace(licenza.begin(), licenza.end(), 'Z', 'X');
+	cout << "Licenza : " << licenza.substr(0, 15) << endl;
 	system("pause");
     	return 0;
 }
